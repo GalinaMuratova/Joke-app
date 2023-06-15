4197640171f52a80f496670d978d332c1ec6dc1d
GalinaMuratova <ufkz200147@gmail.com>
2023-06-15 17:14:21 +0600

    Добавила хук useEffect в нем сделала запрос по ссылке и отобразила данные на странице

--- src/containers/LevelTwo/LevelTwo.tsx
+++ src/containers/LevelTwo/LevelTwo.tsx
@@ -1,14 +1,27 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import './LevelTwo.css'
 
 const LevelTwo = () => {
     const [joke, setJoke] = useState('');
 
     const url = 'https://api.chucknorris.io/jokes/random';
+
+    useEffect(() => {
+        const fetchData = async () => {
+            const response = await fetch(url);
+
+            if (response.ok) {
+                const newJoke = await response.json();
+                setJoke(newJoke.value);
+            }
+        };
+        fetchData().catch(e => console.error(e));
+    }, []);
+
     return (
         <div className="container">
-            <h1>Best Chuck Norris`s jokes :</h1>
-            <p>Some text</p>
+            <h2>Best Chuck Norris`s jokes :</h2>
+            <p>{joke}</p>
             <button type="button" className="btn-next">Next</button>
         </div>
     );
