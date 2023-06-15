9feb08962834f8afc95ea6030aca4b8efd35b5e0
GalinaMuratova <ufkz200147@gmail.com>
2023-06-15 17:36:30 +0600

    Создала функцию для запроса новой шутки по клику на кнопку.

--- src/containers/LevelTwo/LevelTwo.tsx
+++ src/containers/LevelTwo/LevelTwo.tsx
@@ -18,11 +18,24 @@ const LevelTwo = () => {
         fetchData().catch(e => console.error(e));
     }, []);
 
+    const getNewJoke = async () => {
+        try {
+            const response = await fetch(url);
+            if (response.ok) {
+                const newJoke = await response.json();
+                setJoke(newJoke.value);
+            }
+        } catch (e) {
+            console.error(e);
+        }
+    };
+
+
     return (
         <div className="container">
             <h2>Best Chuck Norris`s jokes :</h2>
             <p>{joke}</p>
-            <button type="button" className="btn-next">Next</button>
+            <button type="button" className="btn-next" onClick={getNewJoke}>Next</button>
         </div>
     );
 };
